069b38c58cd6e15ea6e95d8bb9eb370250a35e25
German Gallardo <ggallardoh.90@gmail.com>
2020-10-06 17:13:44 -0300

    Event endpoints

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,29 +1,48 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show, :update, :destroy]
+  before_action :set_event, only: [:view, :buy]
 
   # GET /events/:id
   def view
-    @events = Event.all
-
-    render json: {"response": "view"}
+    if(@event.state == "published")
+      render json: @event.to_json(only: [:name, :description, :start_date, :available_tickets, :price, :image]), content_type: "application/vnd.api+json"
+    else
+      render json: {"errors": {"description":["Event does not exist or unavailable"]}}, status: :bad_request, content_type: "application/vnd.api+json"
+    end
   end
 
   # GET /events
   def list
-    render json: {"response": "list"}
-    #render json: @event
+    @events = Event.select(:id, :name, :start_date, :available_tickets, :price).where(state: "published").where("start_date < ?", (DateTime.now + 1.week)).where("start_date > ?", DateTime.now)
+    render json: @events, content_type: "application/vnd.api+json"
   end
 
   # POST /event/:id/buy
   def buy
-    @event = Event.new(event_params)
+    if (@event.blank? || @event.state != "published")
+      render json: {"errors": {"description":["Event does not exist or unavailable"]}}, status: :bad_request, content_type: "application/vnd.api+json"
+    elsif @event.available_tickets < 1
+      render json: {"errors": {"description":["Tickets sold out"]}}, status: :bad_request, content_type: "application/vnd.api+json"
+    else
+      @ticket = Ticket.new(ticket_params)
+      @ticket.event_id = params[:id]
 
-    render json: {"response": "buy"}
-    # if @event.save
-    #   render json: @event, status: :created, location: @event
-    # else
-    #   render json: @event.errors, status: :unprocessable_entity
-    # end
+      loop do
+        @ticket.confirmation_code = SecureRandom.alphanumeric(10)
+
+        if Ticket.find_by(confirmation_code: @ticket.confirmation_code).nil?
+          break
+        end
+      end
+
+      if @ticket.save
+        @event.update(:available_tickets => (@event.available_tickets - 1))
+        render json: @ticket, content_type: "application/vnd.api+json"
+      else
+        render json: {"errors": @ticket.errors}, status: :bad_request, content_type: "application/vnd.api+json"
+      end  
+    end
+
+
   end
 
   private
@@ -35,5 +54,11 @@ class EventsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def event_params
       params.require(:event).permit(:client_id, :name, :description, :image, :slug, :start_date, :publish_date, :state, :available_tickets, :price)
+    end
+
+    def ticket_params
+      params.require(:data)
+            .require(:attributes)
+            .permit(:buyer_name, :buyer_email)
     end
 end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,4 @@
 class Ticket < ApplicationRecord
   belongs_to :event
+  validates_presence_of :buyer_name, :buyer_email
 end
