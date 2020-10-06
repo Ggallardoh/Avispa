d494839ce00c68d96664f060e3cd2c3eda5c2299
German Gallardo <ggallardoh.90@gmail.com>
2020-10-06 13:47:24 -0300

    Client endpoints

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,32 +1,49 @@
 class ClientsController < ApplicationController
-  before_action :set_client, only: [:show, :update, :destroy]
+  before_action :set_client, only: [:list]
 
   # GET /clients/:id/events
   def list
-    @clients = Client.all
-
-    render json: @clients
+    @events = Event.select("'#{Event.model_name.plural}' as \"type\"", :id, :name, :start_date, :available_tickets, :price).where(client_id: params[:id])
+    
+    render json: {"data": @events}, content_type: "application/vnd.api+json"
   end
 
   # POST /client/:id/events
   def new
-    @client = Client.new(client_params)
+    @event = Event.new(event_params)
 
-    if @client.save
-      render json: @client, status: :created, location: @client
+    @event.state = "created"
+    @event.client_id = params[:id]
+    @event.publish_date = DateTime.now
+
+    if(@event.valid?)
+
+      @event.slug = DateTime.parse(event_params[:start_date]).strftime('%Y-%m-%d')+"-"+event_params[:name].parameterize
+
+      slug_counter = 0
+      while Event.find_by(slug: @event.slug) do
+
+        slug_counter += 1
+        @event.slug += ("-"+slug_counter.to_s)
+      
+      end
+    end
+
+    if @event.save
+      render json: @event, status: :created, location: @client, content_type: "application/vnd.api+json"
     else
-      render json: @client.errors, status: :unprocessable_entity
+      render json: {"errors": @event.errors}, status: :bad_request, content_type: "application/vnd.api+json"
     end
   end
 
   # POST /clients/:id/events/:event_id/publish
   def publish
-    @client = Client.new(client_params)
+    @event = Event.where(id: params[:event_id], client_id: params[:id])
 
-    if @client.save
-      render json: @client, status: :created, location: @client
+    if @event.exists? && @event.update(state: "published")
+      render json: @event, status: :accepted, location: @client, content_type: "application/vnd.api+json"
     else
-      render json: @client.errors, status: :unprocessable_entity
+      render json: {"errors": {"description":["Event does not exist"]}}, status: :bad_request, content_type: "application/vnd.api+json"
     end
   end
 
@@ -39,5 +56,11 @@ class ClientsController < ApplicationController
     # Only allow a trusted parameter "white list" through.
     def client_params
       params.require(:client).permit(:name)
+    end
+
+    def event_params
+      params.require(:data)
+            .require(:attributes)
+            .permit(:name, :start_date, :description, :image, :available_tickets, :price)
     end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   belongs_to :client
+  validates_presence_of :name, :start_date, :description, :image, :available_tickets, :price
 end
